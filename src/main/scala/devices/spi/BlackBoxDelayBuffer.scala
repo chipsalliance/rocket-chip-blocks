chipsalliance/rocket-chip-blocks

commit 09afa7a0e75299824bc0f5e09f01734124128343

add full license boilerplate to scala sources

src/main/scala/devices/spi/BlackBoxDelayBuffer.scala
@@ -1,4 +1,3 @@
-package sifive.blocks.devices.spi
 
 import Chisel._
 import freechips.rocketchip.util.ShiftRegInit
@@ -11,3 +10,19 @@ class BlackBoxDelayBuffer extends BlackBox {
   val mux_out = UInt(OUTPUT, 1.W)
   })
 }
+
+/*
+   Copyright 2016 SiFive, Inc.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/

commit 326c29beb139cdc20b6a0c07fc167e6f7afdbb7c

fix a bug introduced in 09afa7a

src/main/scala/devices/spi/BlackBoxDelayBuffer.scala
@@ -1,3 +1,4 @@
+package sifive.blocks.devices.spi
 
 import Chisel._
 import freechips.rocketchip.util.ShiftRegInit