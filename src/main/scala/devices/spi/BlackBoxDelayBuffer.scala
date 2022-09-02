e6e38267ad977c9dd441806313133c4f9bdb9bb1
Jerry Zhao <jerryz123@berkeley.edu>
2022-09-01 17:28:41 -0700

    Add missing package identifier

--- src/main/scala/devices/spi/BlackBoxDelayBuffer.scala
+++ src/main/scala/devices/spi/BlackBoxDelayBuffer.scala
@@ -1,10 +1,11 @@
+package sifive.blocks.devices.spi
 
 import Chisel._
 import freechips.rocketchip.util.ShiftRegInit
 
 class BlackBoxDelayBuffer extends BlackBox {
   val io = IO(new Bundle() {
-  val in = UInt(INPUT,1.W) 
+  val in = UInt(INPUT,1.W)
   val sel = UInt(INPUT,5.W)
   val out = UInt(OUTPUT, 1.W)
   val mux_out = UInt(OUTPUT, 1.W)
