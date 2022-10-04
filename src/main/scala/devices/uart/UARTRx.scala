413535205ce58dd437d825a35dfa7f3556e8031d
Yanqi Yang <shxxyyq0000@qq.com>
2022-10-04 16:04:53 +0800

    UART refactor v1.1

--- src/main/scala/devices/uart/UARTRx.scala
+++ src/main/scala/devices/uart/UARTRx.scala
@@ -1,6 +1,7 @@
 package sifive.blocks.devices.uart
 
 import chisel3._
+import chisel3.util._
 import freechips.rocketchip.util.CompileOptions.NotStrictInferReset
 
 import freechips.rocketchip.util._
